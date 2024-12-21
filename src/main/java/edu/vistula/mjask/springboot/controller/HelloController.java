d5f84f6d0508322fde6a804cd3f741e2c231bc3d
Mikołaj Jaskulski <miki.jaskolka@gmail.com>
2024-12-21 15:43:12 +0100

    Initial commit

---
+++ src/main/java/edu/vistula/mjask/springboot/controller/HelloController.java
@@ -0,0 +1,24 @@
+package edu.vistula.mjask.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
+@Controller
+public class HelloController {
+
+    @GetMapping(value = "/")
+    public String sayHello() {
+        return "Hello World";
+    }
+
+    @GetMapping(value = "/greeting")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+//    http://localhost:8080/greeting?name=Vistula
+}
